f908bf40431a15ad1c93af9e9824e6a53f30a205
Matheus Cumpian <matheus.cumpian@hotmail.com>
2019-05-24 00:59:21 -0300

    Finalizando implementações das threads e semáforos na função rotaciona.

--- ProjetoGrupo/projeto.c
+++ ProjetoGrupo/projeto.c
@@ -1,77 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
+#include <stdio.h>#include <stdlib.h>#include <pthread.h>FILE* fptr; //Arquivo de EntradaFILE* rot; //Arquivo de Saídapthread_mutex_t mutex_leitura = PTHREAD_MUTEX_INITIALIZER;pthread_mutex_t mutex_escrita = PTHREAD_MUTEX_INITIALIZER;struct arg{	double **matriz;	int linhas;	int colunas;};void* rotaciona(void* arg);int main (int argc, char* argv[]){	int linha = atoi(argv[1]); //numero de linhas	int coluna = atoi(argv[2]); //numero de colunas	int num_threads = atoi(argv[3]); //numero de threads	rot=fopen(argv[5],"w"); //arq1 recebe o nome do arquivo .root que vai ser criado	fptr=fopen(argv[4],"r"); //arq1 recebe o nome do arquivo .dat que vai ser lido	struct arg t_arg;	t_arg.matriz = (double **)malloc(linha * sizeof(double *));   	for (int i=0; i<linha; i++)         	t_arg.matriz[i] = (double *)malloc(coluna * sizeof(double));
 
-
-void* rotaciona(void* arg)
-{
-	
-}
-
-struct arg
-{
-	double **matriz;
-	int linhas;
-	int colunas;
-};
-
-int main (int argc, char* argv[]){
-
-	FILE* fptr; //Arquivo de Entrada
-	FILE* rot; //Arquivo de Saída
-	int linha = atoi(argv[1]); //numero de linhas
-	int coluna = atoi(argv[2]); //numero de colunas
-	int num_threads = atoi(argv[3]); //numero de threads
-	rot=fopen(argv[5],"w"); //arq1 recebe o nome do arquivo .root que vai ser criado
-	fptr=fopen(argv[4],"r"); //arq1 recebe o nome do arquivo .dat que vai ser lido
-	int num;
-	struct arg t_arg;
-	
-	double **mat = (double **)malloc(linha * sizeof(double *)); 
-   	for (int i=0; i<coluna; i++) 
-         	mat[i] = (double *)malloc(coluna * sizeof(double)); 
-	
-	t_arg.matriz = mat;
-
-	printf("I = %d J = %d T = %d\n", linha,coluna,num_threads);
-
-	printf("Arquivo.root = %s\n", argv[argc-1]);
-
-	printf("Arquivo.dat = %s\n", argv[argc-2]);
-
-	pthread_t tids[num_threads];
-	for (int i = 0; i < num_threads; i++)
-	{
-		pthread_create(&tids[i], NULL, rotaciona, &t_arg);
-	}
-	for(int i = 0; i < linha; i++)//linha
-	{
-		for(int j = 0; j < coluna; j++)//coluna
-		{
-
-			fscanf(fptr, "%lf", &mat[i][j]);
-
-		}
-	}
-	printf("matriz lida \n");
-	printf("Girando matriz\n");
-
-	for(int j = 0; j < coluna; j++)//coluna aumenta 1
-	{
-		for(int i=linha-1 ; i >=0; i--)//linha diminiu 
-		{
-			fprintf(rot,"%.2lf ",mat[i][j]);
-		}
-		fprintf(rot,"\n");
-	}
-
-	for(int i = 0; i < num_threads; i++)
-	{
-		pthread_join(tids[i], NULL);
-	}
-
-	fclose(fptr);
-	fclose(rot);
-
-	return(0);
-	}
+    t_arg.linhas = linha;	t_arg.colunas = coluna;	//printf("I = %d J = %d T = %d\n", linha,coluna,num_threads);	//printf("Arquivo.root = %s\n", argv[argc-1]);	//printf("Arquivo.dat = %s\n", argv[argc-2]);	pthread_t tids[num_threads];	for (int i = 0; i < num_threads; i++)	{		printf("criando thread: %d\n", i);		pthread_create(&tids[i], NULL, rotaciona, &t_arg);	}	for(int i = 0; i < num_threads; i++)	{		pthread_join(tids[i], NULL);		printf("Terminando thread: %d\n", i);	}	fclose(fptr);	fclose(rot);	return(0);}void* rotaciona(void* arg){
+	struct arg* struct_pointer = (struct arg*) arg;	for(int i = 0; i < struct_pointer -> linhas; i++)//linha	{		for(int j = 0; j < struct_pointer -> colunas; j++)//coluna		{
+            pthread_mutex_trylock(&mutex_leitura);			fscanf(fptr, "%lf", &struct_pointer->matriz[i][j]);
+			pthread_mutex_unlock(&mutex_leitura);		}	}	printf("matriz lida \n");	printf("Girando matriz\n");	for(int j = 0; j < struct_pointer -> colunas; j++)//coluna aumenta 1	{
+		for(int i=struct_pointer -> linhas-1 ; i >=0; i--)//linha diminiu		{
+            pthread_mutex_trylock(&mutex_escrita);			fprintf(rot,"%.2lf ",struct_pointer -> matriz[i][j]);
+		}		fprintf(rot,"\n");
+		pthread_mutex_unlock(&mutex_escrita);	}	pthread_exit(0);}
